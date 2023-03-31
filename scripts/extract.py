eb26095aef1aa915bd0f1df30fde2723964f705f
deadex-ng <thisisfumba@gmail.com>
2023-03-31 17:36:44 +0200

    update dvc pipeline
    Signed-off-by: deadex-ng <thisisfumba@gmail.com>

--- scripts/extract.py
+++ scripts/extract.py
@@ -4,8 +4,10 @@ import zipfile
 import io
 import os
 
-#URL = "https://databasin2-filestore.s3.amazonaws.com:443/a4cb6d367eae4e52a08902874f8bfedf/download/a4cb6d367eae4e52a08902874f8bfedf_1_zip_en.zip?Signature=O6QSoOR%2BisIRVE2mpxzkphTkhmw%3D&Expires=1680100356&AWSAccessKeyId=AKIAI4RK5BEPK3FCQPUQ"
-URL = "https://zenodo.org/record/5848610/files/biome_cluster_shapefile.zip?download=1"
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", help="url to download data")
+args = vars(parser.parse_args())
+
 def ensure_url_is_accessible(URL):
     r = requests.get(URL)
     if not r.ok:
@@ -13,9 +15,10 @@ def ensure_url_is_accessible(URL):
     else:
         download_and_unzip_files(r.content)
 
+
 def download_and_unzip_files(content):
     current_directory = os.getcwd()
-    target_parent_dir = os.path.join(current_directory, r'tmp_unzip_path')
+    target_parent_dir = os.path.join(current_directory, r"tmp_unzip_path")
     if not os.path.exists(target_parent_dir):
         os.mkdir(target_parent_dir)
     try:
@@ -26,4 +29,5 @@ def download_and_unzip_files(content):
     else:
         print("unzipped successfully")
 
-ensure_url_is_accessible(URL)
+
+ensure_url_is_accessible(args.get("url"))
--- scripts/transform.py
+++ scripts/transform.py
@@ -1,54 +1,62 @@
-# A script to rasterise a shapefile to the same projection & pixel resolution as a reference image.
+# Rasterise a shapefile to the same projection & pixel resolution as a reference image.
 from osgeo import ogr, gdal
 import subprocess
 import os
 
 ogr.UseExceptions()
-gdalformat = 'GTiff'
+gdalformat = "GTiff"
 datatype = gdal.GDT_Byte
-burn_val = 1 #value for the output image pixels
+burn_val = 1
 
-
-output_image = 'ProbaV_UTM_LC100_biome_clusters_V3_global.tif'
+output_image = "ProbaV_UTM_LC100_biome_clusters_V3_global.tif"
 current_directory = os.getcwd()
-input_vector = os.path.join(current_directory, r'tmp_unzip_path/biome_cluster_shapefile/ProbaV_UTM_LC100_biome_clusters_V3_global.shp')
-ref_image = os.path.join(current_directory, r'ref_data/WaterScarcity_GAEZ.tif')
+input_vector = os.path.join(
+    current_directory,
+    r"tmp_unzip_path/biome_cluster_shapefile/ProbaV_UTM_LC100_biome_clusters_V3_global.shp",
+)
+ref_image = os.path.join(current_directory, r"ref_data/WaterScarcity_GAEZ.tif")
+
 
 def read_files(input_vector, ref_image):
     if not os.path.exists(input_vector):
         print("Input vector does not exist")
     else:
-        Image = gdal.Open(ref_image, gdal.GA_ReadOnly)
-        if Image == None:
+        image = gdal.Open(ref_image, gdal.GA_ReadOnly)
+        if image == None:
             print("Unable to read the reference data file")
         else:
             try:
                 Shapefile = ogr.Open(input_vector)
                 if Shapefile:
                     Shapefile_layer = Shapefile.GetLayer()
-                    convert_shp_to_tiff(Image,output_image,Shapefile_layer)
+                    convert_shp_to_tiff(image, output_image, Shapefile_layer)
                 else:
                     print("Couldn't load shapefile")
             except Exception as e:
                 print(e)
 
-def convert_shp_to_tiff(Image,output_image,Shapefile_layer):
-    print("Rasterising shapefile...")
-    Output = gdal.GetDriverByName(gdalformat).Create(output_image, Image.RasterXSize,
-    Image.RasterYSize, 1, datatype, options=['COMPRESS=DEFLATE'])
-    Output.SetProjection(Image.GetProjectionRef())
-    Output.SetGeoTransform(Image.GetGeoTransform())
 
-    # Write data to band 1
+def convert_shp_to_tiff(image, output_image, Shapefile_layer):
+    print("Rasterising shapefile...")
+    Output = gdal.GetDriverByName(gdalformat).Create(
+        output_image,
+        image.RasterXSize,
+        image.RasterYSize,
+        1,
+        datatype,
+        options=["COMPRESS=DEFLATE"],
+    )
+    Output.SetProjection(image.GetProjectionRef())
+    Output.SetGeoTransform(image.GetGeoTransform())
+
     Band = Output.GetRasterBand(1)
     Band.SetNoDataValue(0)
     gdal.RasterizeLayer(Output, [1], Shapefile_layer, burn_values=[burn_val])
 
-    # Close datasets
     Band = None
     Output = None
-    Image = None
+    image = None
     Shapefile = None
 
-read_files(input_vector, ref_image)
 
+read_files(input_vector, ref_image)
