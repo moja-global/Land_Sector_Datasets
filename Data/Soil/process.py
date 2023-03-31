69b3dda9cb7367c5563fe7270b604e504e5bfa59
Bhumi <bhumikarao1506@gmail.com>
2023-03-31 12:07:55 +0530

    workflow
    Signed-off-by: Bhumi <bhumikarao1506@gmail.com>
    Signed-off-by: Bhumika Rao <bhumikarao1506@gmail.com>

---
+++ Data/Soil/process.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+import wget
+import zipfile
+import datetime
+
+# implement pip as a subprocess:
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+#'gdal-utils'])
+
+
+
+
+zipfile.ZipFile('Global Soil Organic Carbon Density in kg Carbon_m2 to 1 meter depth.zip').extractall()
+
+zipfile.ZipFile('Global Soil Organic Carbon Density in kg Carbon_m2 to 1 meter depth.zip').close()
+
+a_file = open("process.py", "r")
+value = datetime.datetime.now()
+date_string = value.strftime('# %Y-%m-%d %H-%M-%S.%f')
+list_of_lines = a_file.readlines()
+list_of_lines[25] = date_string
+a_file = open("process.py", "w")
+a_file.writelines(list_of_lines)
+a_file.close()
+# 2023-03-23 23-02-55.159688
