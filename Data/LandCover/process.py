c1a90cc856f47f533b55858dc9624f2568887ede
ImaJin <imajinarts14@gmail.com>
2023-04-07 16:50:13 +0100

    Task 4- Data Hunter

---
+++ Data/LandCover/process.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+import wget
+import zipfile
+import datetime
+
+# implement pip as a subprocess:
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+#'gdal-utils'])
+
+
+
+wget.download('https://zenodo.org/record/5879022/files/FML_v3-2_with-colorbar.tif?download=1')
+
+
+
+
+a_file = open("process.py", "r")
+value = datetime.datetime.now()
+date_string = value.strftime('# %Y-%m-%d %H-%M-%S.%f')
+list_of_lines = a_file.readlines()
+list_of_lines[25] = date_string
+a_file = open("process.py", "w")
+a_file.writelines(list_of_lines)
+a_file.close()
+
