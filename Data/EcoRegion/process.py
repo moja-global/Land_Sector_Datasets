7b142d64a33e8e3e4a9cd60e014247bdcf87f567
ImaJin <imajinarts14@gmail.com>
2023-04-07 16:39:46 +0100

    Task 5- Automate curation of land sector datasets

---
+++ Data/EcoRegion/process.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+import wget
+import zipfile
+import datetime
+
+# implement pip as a subprocess:
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+#'gdal-utils'])
+
+wget.download('https://drive.google.com/uc?export=download&id=1K4nd7alzdwMQ4lOzC8wNewKU61dxncnr')
+
+
+
+
+
+
+a_file = open("process.py", "r")
+value = datetime.datetime.now()
+date_string = value.strftime('# %Y-%m-%d %H-%M-%S.%f')
+list_of_lines = a_file.readlines()
+list_of_lines[25] = date_string
+a_file = open("process.py", "w")
+a_file.writelines(list_of_lines)
+a_file.close()
+
