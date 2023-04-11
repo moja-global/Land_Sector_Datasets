dcac140f563dea1b5eb7323f9976cd92bb798e94
ImaJin <imajinarts14@gmail.com>
2023-04-11 20:36:51 +0100

    repeat task 3 with different dataset(IPCC_ClimateZoneMap) and updated link for task 3 dataset(GlobalSoilOrganicCarbonDensityinkgCarbon_m2to1meterdepth)

---
+++ Data/Climate/process.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+import wget
+import zipfile
+import datetime
+
+# implement pip as a subprocess:
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+#'gdal-utils'])
+
+wget.download('https://esdac.jrc.ec.europa.eu/projects/RenewableEnergy/Data/Climate_Zone.zip')
+
+
+zipfile.ZipFile('Climate_Zone.zip').extractall()
+
+zipfile.ZipFile('Climate_Zone.zip').close()
+
+a_file = open("process.py", "r")
+value = datetime.datetime.now()
+date_string = value.strftime('# %Y-%m-%d %H-%M-%S.%f')
+list_of_lines = a_file.readlines()
+list_of_lines[25] = date_string
+a_file = open("process.py", "w")
+a_file.writelines(list_of_lines)
+a_file.close()
+
