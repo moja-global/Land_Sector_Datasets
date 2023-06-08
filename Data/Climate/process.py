9a57b3e5de247238997ba8c2a67374057db7d408
ImaJin <imajinarts14@gmail.com>
2023-06-08 06:24:50 +0100

    Updated dataset on Thu 08 Jun 2023 06:24:50 AM WAT with GitHub Actions

--- Data/Climate/process.py
+++ Data/Climate/process.py
@@ -10,16 +10,12 @@ import os
 zip_url = 'https://esdac.jrc.ec.europa.eu/projects/RenewableEnergy/Data/Climate_Zone.zip'
 zip_file_path = wget.download(zip_url)
 
-# Extract the zip file to a folder with the same name
-folder_path = os.path.splitext(zip_file_path)[0]
-os.makedirs(folder_path, exist_ok=True)
+zipfile.ZipFile('Climate_Zone.zip').extractall()
 
-with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall(folder_path)
-
+zipfile.ZipFile('Climate_Zone.zip').close()
 
 # Convert from .rst to tif and apply WGS84 and resample to 0.005
-gdalwarp_cmd = 'gdalwarp -t_srs EPSG:4326 -dstnodata 255.0 -tr 0.005 0.005 -r near -te -180.0 -90.0 180.0 90.0 -te_srs EPSG:4326 -of GTiff Climate_Zone/CLIMATE_ZONE.rst IPCC_ClimateZoneMap.tif'
+gdalwarp_cmd = 'gdalwarp -t_srs EPSG:4326 -dstnodata 255.0 -tr 0.005 0.005 -r near -te -180.0 -90.0 180.0 90.0 -te_srs EPSG:4326 -of GTiff CLIMATE_ZONE.rst IPCC_ClimateZoneMap.tif'
 os.system(gdalwarp_cmd)
 
 # Convert to polygon
